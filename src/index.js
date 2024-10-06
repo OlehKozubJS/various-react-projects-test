cc4a665ed78fc66f4768a8474621f51df5153227
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-06 22:04:44 +0300

    add main files to project
    add such files as:
    .github\workflows\main.yml
    .gitignore
    public\index.html
    src\index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom";
+
+const htmlRoot = document.getElementById("root");
+const reactRoot = createRoot(htmlRoot);
+
+reactRoot.render(<StrictMode>Hello!</StrictMode>);
