66e38ff1cf174aa80236a9093c8d41620a50a3f1
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-07 12:42:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 
+import { App } from "./App";
+
 const htmlRoot = document.getElementById("root");
 const reactRoot = createRoot(htmlRoot);
 
-reactRoot.render(<StrictMode></StrictMode>);
+reactRoot.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
