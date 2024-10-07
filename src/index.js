5b600d9f84a6e60ec8f9bb2a59d501d5cc5d9508
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-07 12:40:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,4 @@ import { createRoot } from "react-dom";
 const htmlRoot = document.getElementById("root");
 const reactRoot = createRoot(htmlRoot);
 
-reactRoot.render(
-  <StrictMode>
-    Hello!
-    <br />
-    I am VioletFox!
-    <br />
-    Progress Wins Again!
-    <br />
-    Keroberos
-  </StrictMode>
-);
+reactRoot.render(<StrictMode></StrictMode>);
