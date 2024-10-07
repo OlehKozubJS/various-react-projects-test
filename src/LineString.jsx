32230cb82bcb766a53ced01397b5a33506d8191e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-07 13:00:58 +0300

    Update LineString.jsx

--- src/LineString.jsx
+++ src/LineString.jsx
@@ -1,5 +1,7 @@
-const LineString = ({ x1, y1, x2, y2 }) => {
-  return <polyline points={`${x1} ${y1} ${x2} ${y2}`}></polyline>;
+const LineString = ({ x1, y1, x2, y2, stroke }) => {
+  return (
+    <polyline points={`${x1} ${y1} ${x2} ${y2}`} stroke={stroke}></polyline>
+  );
 };
 
 export { LineString };
