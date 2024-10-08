202474a9e5856598d082c58739a878b55a6ab289
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-09 00:16:51 +0300

    Update TurtleComponent.jsx

--- src/SVGComponents/TurtleComponent.jsx
+++ src/SVGComponents/TurtleComponent.jsx
@@ -1,13 +1,19 @@
-function turtle(x, y, turn, upOrDown) {
-  var polygonArray = [];
-  polygonArray.push(angleFunction(x, y, 10, -90 + turn));
-  polygonArray.push(angleFunction(x, y, 30, turn));
-  polygonArray.push(angleFunction(x, y, 10, 90 + turn));
-  var polygonString =
-    "<polygon class='turtle " +
-    upOrDown +
-    "' points='" +
-    polygonArray.join(",") +
-    "'></polygon>";
-  return polygonString;
-}
+import { angleFunction } from "./angleFunctions";
+
+import { LineComponent } from "./LineComponent";
+
+const TurtleComponent = (x, y, turn, backgroundColor, borderColor) => {
+  return (
+    <LineComponent
+      points={[
+        angleFunction(x, y, 10, -90 + turn),
+        angleFunction(x, y, 30, turn),
+        angleFunction(x, y, 10, 90 + turn),
+      ]}
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+    />
+  );
+};
+
+export { TurtleComponent };
