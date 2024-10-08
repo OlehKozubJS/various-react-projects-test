447ea8b48143f9622d37c1cabe92f1c941e2fa77
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-09 00:31:52 +0300

    Update TurtleComponent.jsx

--- src/SVGComponents/TurtleComponent.jsx
+++ src/SVGComponents/TurtleComponent.jsx
@@ -6,9 +6,9 @@ const TurtleComponent = (x, y, turn, backgroundColor, borderColor) => {
   return (
     <LineComponent
       points={[
-        angleFunction(x, y, 10, -90 + turn),
-        angleFunction(x, y, 30, turn),
-        angleFunction(x, y, 10, 90 + turn),
+        ...angleFunction(x, y, 10, -90 + turn),
+        ...angleFunction(x, y, 30, turn),
+        ...angleFunction(x, y, 10, 90 + turn),
       ]}
       backgroundColor={backgroundColor}
       borderColor={borderColor}
