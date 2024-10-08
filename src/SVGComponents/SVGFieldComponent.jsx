64da690d7e8e4d96b38ffdff38330f8caa6fb1c5
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-08 23:21:44 +0300

    Update SVGFieldComponent.jsx

---
+++ src/SVGComponents/SVGFieldComponent.jsx
@@ -0,0 +1,19 @@
+const SVGFieldComponent = () => {
+  return (
+    <svg
+      style={{
+        width: "300px",
+        height: "300px",
+        backgroundColor: "rgb(240, 230, 250)",
+      }}
+    >
+      <LineComponent
+        points={[0, 0, 50, 50, 100, 50]}
+        backgroundColor={"none"}
+        borderColor={"red"}
+      />
+    </svg>
+  );
+};
+
+export { App };
