6d9fa23a59fc4b70db2efc3472a90363a5f7331a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-09 00:52:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,20 +38,9 @@ const App = () => {
           backgroundColor={"cyan"}
           borderColor={"rgb(200, 100, 180)"}
         />
-        <TurtleComponent
-          x={215}
-          y={230}
-          turn={60}
-          backgroundColor={"rgb(60, 60, 70)"}
-          borderColor={"rgb(30, 30, 70)"}
-        />
       </SVGFieldComponent>
       <LineComponent
-        points={[
-          ...angleFunction(215, 230, 10, -90 + 60),
-          ...angleFunction(215, 230, 30, 60),
-          ...angleFunction(215, 230, 10, 90 + 60),
-        ]}
+        points={[0, 0, 50, 70]}
         backgroundColor={"rgb(60, 60, 70)"}
         borderColor={"rgb(30, 30, 70)"}
       />
@@ -65,3 +54,12 @@ const App = () => {
 };
 
 export { App };
+/**
+ *         <TurtleComponent
+          x={215}
+          y={230}
+          turn={60}
+          backgroundColor={"rgb(60, 60, 70)"}
+          borderColor={"rgb(30, 30, 70)"}
+        />
+ */
