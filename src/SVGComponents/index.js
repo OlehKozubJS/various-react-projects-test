00a3d3826c605e20f490c3bce9662b64402866c8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-08 23:25:19 +0300

    Create index.js

---
+++ src/SVGComponents/index.js
@@ -0,0 +1,5 @@
+import { SVGFieldComponent } from "./SVGFieldComponent";
+import { LineComponent } from "./LineComponent";
+//import {} from "./RadiantLineComponent";
+
+export { SVGFieldComponent, LineComponent };
