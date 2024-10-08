c6ab1aafe4083595e0383821ce1dcbd85f5e9de0
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-09 00:04:30 +0300

    Create angleFunctions.js

---
+++ src/SVGComponents/angleFunctions.js
@@ -0,0 +1,13 @@
+const sinFunc = (angle) => {
+  return Math.sin((Math.PI / 180) * angle);
+};
+
+const cosFunc = (angle) => {
+  return Math.cos((Math.PI / 180) * angle);
+};
+
+const angleFunction = (x, y, length, angle) => {
+  return [x + length * cosFunc(angle), y + l * sinFunc(angle)];
+};
+
+export { sinFunc, cosFunc, angleFunction };
