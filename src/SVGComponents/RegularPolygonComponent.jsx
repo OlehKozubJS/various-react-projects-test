8f8f654c46e0eaec757bf9687b54a8e205e79dda
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-09 00:02:56 +0300

    Update RegularPolygonComponent.jsx

--- src/SVGComponents/RegularPolygonComponent.jsx
+++ src/SVGComponents/RegularPolygonComponent.jsx
@@ -7,6 +7,30 @@ const RegularPolygonComponent = ({ x, y, l, angleNumber, angle }) => {
   var polygonString =
     "<polygon points='" + polygonArray.join(",") + "'></polygon>";
   return polygonString;
+  const sinFunc = (angle) => {
+    return Math.sin((Math.PI / 180) * angle);
+  };
+
+  const cosFunc = (angle) => {
+    return Math.cos((Math.PI / 180) * angle);
+  };
+
+  function angleFunction(x, y, length, angle) {
+    return [x + length * cosFunc(angle), y + l * sinFunc(angle)];
+  }
+
+  return (
+    <LineComponent
+      points={[
+        x,
+        y,
+        angleFunction(x, y, length, angle)[0],
+        angleFunction(x, y, length, angle)[1],
+      ]}
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+    />
+  );
 };
 
 export { RegularPolygonComponent };
