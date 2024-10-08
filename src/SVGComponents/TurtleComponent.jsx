a6325f465b92b0256bc90a2e6d934155c57de835
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-08 23:57:00 +0300

    Update TurtleComponent.jsx

--- src/SVGComponents/TurtleComponent.jsx
+++ src/SVGComponents/TurtleComponent.jsx
@@ -1,14 +1,3 @@
-function regularPolygon(x, y, l, angleNumber, angle) {
-  var polygonArray = [];
-  for (aa = 1; aa <= angleNumber; ++aa) {
-    var xy = angleFunction(x, y, l, (360 / angleNumber) * aa + angle);
-    polygonArray.push(xy);
-  }
-  var polygonString =
-    "<polygon points='" + polygonArray.join(",") + "'></polygon>";
-  return polygonString;
-}
-
 function turtle(x, y, turn, upOrDown) {
   var polygonArray = [];
   polygonArray.push(angleFunction(x, y, 10, -90 + turn));
