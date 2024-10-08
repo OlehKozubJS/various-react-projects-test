dfb5bcb584581553e1102535e488744a3ab1710d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-08 23:57:14 +0300

    Update RegularPolygonComponent.jsx

--- src/SVGComponents/RegularPolygonComponent.jsx
+++ src/SVGComponents/RegularPolygonComponent.jsx
@@ -8,17 +8,3 @@ function regularPolygon(x, y, l, angleNumber, angle) {
     "<polygon points='" + polygonArray.join(",") + "'></polygon>";
   return polygonString;
 }
-
-function turtle(x, y, turn, upOrDown) {
-  var polygonArray = [];
-  polygonArray.push(angleFunction(x, y, 10, -90 + turn));
-  polygonArray.push(angleFunction(x, y, 30, turn));
-  polygonArray.push(angleFunction(x, y, 10, 90 + turn));
-  var polygonString =
-    "<polygon class='turtle " +
-    upOrDown +
-    "' points='" +
-    polygonArray.join(",") +
-    "'></polygon>";
-  return polygonString;
-}
