1c3629714e4e2ff28189edc5e11d3ccf92426923
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-08 23:43:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,9 @@ const App = () => {
   return (
     <>
       <SVGFieldComponent
-        style={{
-          width: "300px",
-          height: "300px",
-          backgroundColor: "rgb(240, 230, 250)",
-        }}
+        width="300px"
+        height="300px"
+        backgroundColor="rgb(240, 230, 250)"
       >
         <LineComponent
           points={[0, 0, 50, 50, 100, 50]}
