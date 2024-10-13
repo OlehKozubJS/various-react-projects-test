413bb63cf232278d2d88da4ff351e03d8c836e2a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-14 00:31:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,6 @@ import {
 } from "./SVGComponents";
 
 const App = () => {
-  let horizontalCoordinates = [];
-
-  for (let x = 0; x <= 1000; x += 10) {
-    horizontalCoordinates = [...horizontalCoordinates, x];
-  }
   const angle = 0;
   const [angleValue, setAngleValue] = useState(0);
   const [initialX, setInitialX] = useState(0);
