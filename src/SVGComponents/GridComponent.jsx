9fc669b367c3c9099b73879f153610654914874c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-14 00:54:38 +0300

    Update GridComponent.jsx

--- src/SVGComponents/GridComponent.jsx
+++ src/SVGComponents/GridComponent.jsx
@@ -4,9 +4,9 @@ const GridComponent = ({ fieldHeight, fieldWidth, cellHeight, cellWidth }) => {
   let horizontalCoordinates = [];
   let verticalCoordinates = [];
 
-for (let y = 0; y <= fieldHeight; y += cellHeight) {
-  verticalCoordinates = [...verticalCoordinates, y];
-}
+  for (let y = 0; y <= fieldHeight; y += cellHeight) {
+    verticalCoordinates = [...verticalCoordinates, y];
+  }
 
   for (let x = 0; x <= fieldWidth; x += cellWidth) {
     horizontalCoordinates = [...horizontalCoordinates, x];
@@ -16,25 +16,24 @@ for (let y = 0; y <= fieldHeight; y += cellHeight) {
     <>
       {horizontalCoordinates.map((item) => {
         return (
-          <>
-            <LineComponent
-              key={item}
-              points={[item, 0, item, 1000]}
-              backgroundColor="blue"
-              borderColor="red"
-            />
-        )
-      }
-      {verticalCoordinates.map((item) =>{
-            return(<LineComponent
-              key={item}
-              points={[0, item, 1000, item]}
-              backgroundColor="blue"
-              borderColor="red"
-            />
-
+          <LineComponent
+            key={item}
+            points={[item, 0, item, 1000]}
+            backgroundColor="blue"
+            borderColor="red"
+          />
         );
-      }
+      })}
+      {verticalCoordinates.map((item) => {
+        return (
+          <LineComponent
+            key={item}
+            points={[0, item, 1000, item]}
+            backgroundColor="blue"
+            borderColor="red"
+          />
+        );
+      })}
     </>
   );
 };
