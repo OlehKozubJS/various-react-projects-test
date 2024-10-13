c0665ba858a402359ae67bbd328aa96b6e76b399
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-14 01:35:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
           borderColor="red"
         />
       </SVGFieldComponent>
-      {(Math.asin(Math.pow(2, 1 / 2) / 2) / Math.PI) * 180}
+      {Math.asin(Math.pow(3, 1 / 2) / 2) / (Math.PI / 180)}
     </>
   );
 };
