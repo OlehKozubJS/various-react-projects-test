69f91fa8c66aece400ef05ed59e49b77d132264a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-14 01:45:02 +0300

    Update TurtleComponent.jsx

--- src/SVGComponents/TurtleComponent.jsx
+++ src/SVGComponents/TurtleComponent.jsx
@@ -1,4 +1,4 @@
-import { angleFunction } from "./angleFunctions";
+import { getXYbyAngle } from "./angleFunctions";
 
 import { LineComponent } from "./LineComponent";
 
@@ -6,9 +6,9 @@ const TurtleComponent = ({ x, y, turn, backgroundColor, borderColor }) => {
   return (
     <LineComponent
       points={[
-        ...angleFunction(x, y, 10, -90 + turn),
-        ...angleFunction(x, y, 30, turn),
-        ...angleFunction(x, y, 10, 90 + turn),
+        ...getXYbyAngle(x, y, 10, -90 + turn),
+        ...getXYbyAngle(x, y, 30, turn),
+        ...getXYbyAngle(x, y, 10, 90 + turn),
       ]}
       backgroundColor={backgroundColor}
       borderColor={borderColor}
