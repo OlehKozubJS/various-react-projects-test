8f4846ea93100290fb89471667b469cb7041520d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-13 23:03:36 +0300

    Update index.js

--- src/SVGComponents/index.js
+++ src/SVGComponents/index.js
@@ -3,7 +3,8 @@ import { LineComponent } from "./LineComponent";
 import { RadiantLineComponent } from "./RadiantLineComponent";
 import { RegularPolygonComponent } from "./RegularPolygonComponent";
 import { TurtleComponent } from "./TurtleComponent";
-
+import { ReversedTurtleComponent } from "./ReversedTurtleComponent";
+import
 export {
   SVGFieldComponent,
   LineComponent,
