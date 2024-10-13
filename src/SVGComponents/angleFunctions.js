f6a0c84cc4efe5fa878634d8145b71cf9acd01ef
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-14 01:39:54 +0300

    Update angleFunctions.js

--- src/SVGComponents/angleFunctions.js
+++ src/SVGComponents/angleFunctions.js
@@ -1,9 +1,15 @@
+const RADIAN = Math.PI / 180;
+
 const sinFunc = (angle) => {
-  return Math.sin((Math.PI / 180) * angle);
+  return Math.sin(angle * RADIAN);
 };
 
 const cosFunc = (angle) => {
-  return Math.cos((Math.PI / 180) * angle);
+  return Math.cos(angle * RADIAN);
+};
+
+const arcSinFunc = (sine) => {
+  return Math.round(Math.asin(sine) / RADIAN);
 };
 
 const angleFunction = (x, y, length, angle) => {
