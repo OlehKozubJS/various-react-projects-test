f25766ca7e537ef4a4ee93d0dc6be35e4109bbc7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-15 21:44:10 +0300

    Create ArrowComponent.jsx

---
+++ src/SVGComponents/ArrowComponent.jsx
@@ -0,0 +1,34 @@
+import { getXYbyAngle } from "./angleFunctions";
+
+import { LineComponent } from "./LineComponent";
+
+const ReversedTurtleComponent = ({
+  x,
+  y,
+  turn,
+  backgroundColor,
+  borderColor,
+  onMouseDown,
+  onMouseMove,
+  onMouseUp,
+}) => {
+  return (
+    <LineComponent
+      points={[
+        x,
+        y,
+        ...getXYbyAngle(x, y, 200, turn),
+        ...getXYbyAngle(x, y, 200, turn + 30),
+        x,
+        y,
+      ]}
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+      onMouseDown={onMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={onMouseUp}
+    />
+  );
+};
+
+export { ReversedTurtleComponent };
