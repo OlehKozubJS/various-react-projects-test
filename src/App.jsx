503444f49946818416e307577abbe4bdcd22be55
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-15 23:54:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,36 @@ import { useState } from "react";
 import { SVGFieldComponent, ArrowComponent } from "./SVGComponents";
 
 const App = () => {
-  return (
-    <>
-      <SVGFieldComponent
-        width="600px"
-        height="600px"
-        backgroundColor="rgb(240, 230, 250)"
-      ></SVGFieldComponent>
-    </>
-  );
-};
-
-export { App };
-
-/*
   const [angle, setAngle] = useState(0);
 
   const handleTurn = (value) => {
     setAngle(value);
   };
 
-
+  return (
+    <div>
+      <SVGFieldComponent
+        width="600px"
+        height="600px"
+        backgroundColor="rgb(240, 230, 250)"
+      >
         <ArrowComponent
-          x={100}
-          y={150}
+          x={300}
+          y={300}
           onTurn={handleTurn}
           backgroundColor={"red"}
           borderColor={"blue"}
         />
+      </SVGFieldComponent>
+      {`angle value: ${angle}`}
+    </div>
+  );
+};
 
-      {`angle value: ${angle}`}    
+export { App };
+
+/*
+
+
+
 */
