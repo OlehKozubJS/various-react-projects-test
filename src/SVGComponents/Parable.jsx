00249b9f5e21f290130beb6200e5601305310ec9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-16 22:27:23 +0300

    Update Parable.jsx

--- src/SVGComponents/Parable.jsx
+++ src/SVGComponents/Parable.jsx
@@ -2,12 +2,18 @@ import { LineComponent } from "./LineComponent";
 
 const Parable = () => {
   let coordinates = [];
-  for (let x = 0; x <= 10; x += 0.1) {
+  for (let x = -10; x <= 10; x += 0.01) {
     const y = Math.pow(x, 2);
-    coordinates = [...coordinates, x * 10, y * 10];
+    coordinates = [...coordinates, 300 + x * 100, 300 - y * 100];
   }
 
-  return <LineComponent points={coordinates} />;
+  return (
+    <LineComponent
+      points={coordinates}
+      backgroundColor={"none"}
+      borderColor={"magenta"}
+    />
+  );
 };
 
 export { Parable };
