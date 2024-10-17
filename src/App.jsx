29fd9a1e3119711f07be9f211e41579a22a6f05c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-17 22:51:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ import {
 } from "./SVGComponents";
 
 const App = () => {
+  const [isSVG, setIsSVG] = useState(false);
+  const openSVG = () => {
+    setIsSVG(true);
+  };
+  const closeSVG = () => {
+    setIsSVG(false);
+  };
+
   const [isArrowComponent, setIsArrowComponent] = useState(false);
   const openArrowComponent = () => {
     setIsArrowComponent(true);
@@ -29,57 +37,82 @@ const App = () => {
   };
   const scale = 60;
 
-  const [is]
+  const [isGridComponent, setIsGridComponent] = useState(false);
+  const openGridComponent = () => {
+    setIsGridComponent(true);
+  };
+  const closeGridComponent = () => {
+    setIsGridComponent(false);
+  };
 
   return (
-    <div>
-      <SVGFieldComponent
-        width="600px"
-        height="600px"
-        backgroundColor="rgb(240, 230, 250)"
-      >
-        {isArrowComponent && (
-          <ArrowComponent
-            x={300}
-            y={300}
-            onTurn={handleTurn}
-            backgroundColor={"red"}
-            borderColor={"blue"}
-          />
-        )}
-        <GridComponent
-          cellHeight={scale / 10}
-          cellWidth={scale / 10}
-          fieldHeight={600}
-          fieldWidth={600}
-          color="rgb(150, 150, 200)"
-        />
-        <GridComponent
-          cellHeight={scale}
-          cellWidth={scale}
-          fieldHeight={600}
-          fieldWidth={600}
-          color="rgb(100, 100, 200)"
-        />
-        {isParable && <Parable scale={scale} />}
-      </SVGFieldComponent>
-      <br />
-      <button
-        type="button"
-        onClick={isArrowComponent ? closeArrowComponent : openArrowComponent}
-      >
-        {isArrowComponent ? "Close arrow component" : "Open arrow component"}
+    <>
+      <button type="button" onClick={isSVG ? closeSVG : openSVG}>
+        {isSVG ? "Close" : "Open"} vector graphic field
       </button>
-      {isArrowComponent && `angle value: ${angle}`}
-      <br />
-      <button type="button" onClick={isParable ? closeParable : openParable}>
-        {isParable ? "Close parable" : "Open parable"}
-      </button>
-      <br />
-      <button>
-        {is}
-      </button>
-    </div>
+      {isSVG && (
+        <>
+          <SVGFieldComponent
+            width="600px"
+            height="600px"
+            backgroundColor="rgb(240, 230, 250)"
+          >
+            {isArrowComponent && (
+              <ArrowComponent
+                x={300}
+                y={300}
+                onTurn={handleTurn}
+                backgroundColor={"red"}
+                borderColor={"blue"}
+              />
+            )}
+            {isGridComponent && (
+              <>
+                <GridComponent
+                  cellHeight={scale / 10}
+                  cellWidth={scale / 10}
+                  fieldHeight={600}
+                  fieldWidth={600}
+                  color="rgb(150, 150, 200)"
+                />
+                <GridComponent
+                  cellHeight={scale}
+                  cellWidth={scale}
+                  fieldHeight={600}
+                  fieldWidth={600}
+                  color="rgb(100, 100, 200)"
+                />
+              </>
+            )}
+            {isParable && <Parable scale={scale} />}
+          </SVGFieldComponent>
+          <br />
+          <button
+            type="button"
+            onClick={
+              isArrowComponent ? closeArrowComponent : openArrowComponent
+            }
+          >
+            {isArrowComponent ? "Close " : "Open "}arrow component
+          </button>
+          {isArrowComponent && `angle value: ${angle}`}
+          <br />
+          <button
+            type="button"
+            onClick={isParable ? closeParable : openParable}
+          >
+            {isParable ? "Close " : "Open "}parable
+          </button>
+          <br />
+          <button
+            type="button"
+            onClick={isGridComponent ? closeGridComponent : openGridComponent}
+          >
+            {isGridComponent ? "Close " : "Open "}grid component
+          </button>
+        </>
+      )}
+    </>
   );
 };
 
