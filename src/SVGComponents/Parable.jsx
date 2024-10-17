10cb51ec51a0ab77370afa82865f08f411ce9bb1
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-18 00:46:03 +0300

    Update Parable.jsx

--- src/SVGComponents/Parable.jsx
+++ src/SVGComponents/Parable.jsx
@@ -3,7 +3,7 @@ import { LineComponent } from "./LineComponent";
 const Parable = ({ scale }) => {
   let coordinates = [];
   for (let x = -10; x <= 10; x += 1 / scale) {
-    const y = Math.sqrt(Math.abs(5 - x * x));
+    const y = Math.sqrt(Math.abs(x * 1));
     coordinates = [...coordinates, 300 + x * scale, 300 - y * scale];
   }
 
