127aa65a463a3c6d6262874066b77d0b33589b83
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-19 00:22:15 +0300

    Update Calculator.jsx

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import css from "./Calculator.module.css";
 
 const Calculator = () => {
-  return <>This is Calulator</>;
+  const [expression, setExpression] = useState("");
+
+  return (
+    <>
+      This is Calulator
+      <div></div>
+      <div>
+        <div>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">.</button>
+          <button type="button">0</button>
+          <button type="button">{"<="}</button>
+        </div>
+        <div>
+          <button type="button">{"("}</button>
+          <button type="button">{")"}</button>
+          <button type="button">+</button>
+          <button type="button">-</button>
+          <button type="button">*</button>
+          <button type="button">/</button>
+          <button type="button">=</button>
+          <button type="button">C</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export { Calculator };
