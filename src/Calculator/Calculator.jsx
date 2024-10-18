ee15f1c7eef781393df64b96bba1ecf742ce8ca2
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-19 00:55:40 +0300

    Update Calculator.jsx

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -4,34 +4,82 @@ import css from "./Calculator.module.css";
 const Calculator = () => {
   const [expression, setExpression] = useState("");
 
+  const enterData = () => {};
+
+  const calculate = () => {};
+
+  const backSpace = () => {};
+
+  const clear = () => {};
+
   return (
     <>
       This is Calulator
       <div></div>
       <div>
         <div>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button">.</button>
-          <button type="button">0</button>
-          <button type="button">{"<="}</button>
+          <button type="button" onClick={enterData("1")}>
+            1
+          </button>
+          <button type="button" onClick={enterData("2")}>
+            2
+          </button>
+          <button type="button" onClick={enterData("3")}>
+            3
+          </button>
+          <button type="button" onClick={enterData("4")}>
+            4
+          </button>
+          <button type="button" onClick={enterData("5")}>
+            5
+          </button>
+          <button type="button" onClick={enterData("6")}>
+            6
+          </button>
+          <button type="button" onClick={enterData("7")}>
+            7
+          </button>
+          <button type="button" onClick={enterData("8")}>
+            8
+          </button>
+          <button type="button" onClick={enterData("9")}>
+            9
+          </button>
+          <button type="button" onClick={enterData(".")}>
+            .
+          </button>
+          <button type="button" onClick={enterData("0")}>
+            0
+          </button>
+          <button type="button" onClick={backSpace()}>
+            {"<="}
+          </button>
         </div>
         <div>
-          <button type="button">{"("}</button>
-          <button type="button">{")"}</button>
-          <button type="button">+</button>
-          <button type="button">-</button>
-          <button type="button">*</button>
-          <button type="button">/</button>
-          <button type="button">=</button>
-          <button type="button">C</button>
+          <button type="button" onClick={enterData("(")}>
+            {"("}
+          </button>
+          <button type="button" onClick={enterData(")")}>
+            {")"}
+          </button>
+          <button type="button" onClick={enterData("+")}>
+            +
+          </button>
+          <button type="button" onClick={enterData("-")}>
+            -
+          </button>
+          <button type="button" onClick={enterData("*")}>
+            *
+          </button>
+          <button type="button" onClick={enterData("/")}>
+            /
+          </button>
+          <button type="button" onClick={calculate()}>
+            =
+          </button>
+          <button type="button" onClick={clear()}>
+            C
+          </button>
         </div>
       </div>
     </>
