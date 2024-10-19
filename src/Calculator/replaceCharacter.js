31ee9704ad208b632153cdfb3a56c04d4b0e7c37
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-20 00:35:57 +0300

    Update replaceCharacter.js

--- src/Calculator/replaceCharacter.js
+++ src/Calculator/replaceCharacter.js
@@ -1,6 +1,6 @@
 const replaceCharacter = (string, index, newCharacter) => {
   const newString = [...string];
-  newString.splice(index, 1, newCharacter);
+  newString.splice(index, 0, newCharacter);
   return newString.join("");
 };
 
