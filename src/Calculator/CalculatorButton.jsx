9e171554d60b0e07f175f9016afd1f767127e288
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-22 21:00:06 +0300

    Update CalculatorButton.jsx

--- src/Calculator/CalculatorButton.jsx
+++ src/Calculator/CalculatorButton.jsx
@@ -1,6 +1,13 @@
+import css from "./Calculator.module.css";
+
 const CalculatorButton = ({ onClick, children }) => {
   return (
-    <button type="button" onClick={onClick} value={children}>
+    <button
+      type="button"
+      onClick={onClick}
+      value={children}
+      className={css.CalculatorButton}
+    >
       {children}
     </button>
   );
