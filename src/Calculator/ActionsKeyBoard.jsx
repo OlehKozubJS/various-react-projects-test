0d5a92cb267df7fa9fd95d5c07096c6a1443bdc6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-22 23:09:03 +0300

    Create ActionsKeyBoard.jsx

---
+++ src/Calculator/ActionsKeyBoard.jsx
@@ -0,0 +1,24 @@
+import { CalculatorButton } from "./CalculatorButton";
+
+import css from "./Calculator.module.css";
+
+const NumberKeyboard = ({ onClick, backSpace }) => {
+  const enterData = (event) => {
+    onClick(event.target.value);
+  };
+
+  return (
+    <div className={css.NumberKeyBoard}>
+      {"123456789.0".split("").map((item) => {
+        return (
+          <CalculatorButton key={item} onClick={enterData}>
+            {item}
+          </CalculatorButton>
+        );
+      })}
+      <CalculatorButton onClick={backSpace}>◄</CalculatorButton>
+    </div>
+  );
+};
+
+export { NumberKeyboard };
