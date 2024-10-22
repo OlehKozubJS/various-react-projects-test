f170214f63e6652755199bb9fba53d7418caa106
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-22 21:28:51 +0300

    Update NumberKeyBoard.jsx

--- src/Calculator/NumberKeyBoard.jsx
+++ src/Calculator/NumberKeyBoard.jsx
@@ -1,48 +1,24 @@
-const NumberKeyboard = () => {
-    return(        <div>
-          <button
-            type="button"
-            onClick={enterData}
-            value="1"
-            className={css.CalculatorButton}
-          >
-            1
-          </button>
-          <button type="button" onClick={enterData} value="2">
-            2
-          </button>
-          <button type="button" onClick={enterData} value="3">
-            3
-          </button>
-          <button type="button" onClick={enterData} value="4">
-            4
-          </button>
-          <button type="button" onClick={enterData} value="5">
-            5
-          </button>
-          <button type="button" onClick={enterData} value="6">
-            6
-          </button>
-          <button type="button" onClick={enterData} value="7">
-            7
-          </button>
-          <button type="button" onClick={enterData} value="8">
-            8
-          </button>
-          <button type="button" onClick={enterData} value="9">
-            9
-          </button>
-          <button type="button" onClick={enterData} value=".">
-            .
-          </button>
-          <button type="button" onClick={enterData} value="0">
-            0
-          </button>
-          <button type="button" onClick={backSpace}>
-            {"<="}
-          </button>
-        </div>);
+import { CalculatorButton } from "./CalculatorButton";
+
+const NumberKeyboard = ({ onClick, backSpace }) => {
+  enterData = (event) => {
+    onClick(event.target.value);
+  };
+
+  return (
+    <div>
+      {["1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0"].map((item) => {
+        return (
+          <CalculatorButton key={item} onClick={enterData}>
+            {item}
+          </CalculatorButton>
+        );
+      })}
+      <button type="button" onClick={backSpace}>
+        {"<="}
+      </button>
+    </div>
+  );
 };
 
-
-export (NumberKeyboard);
+export { NumberKeyboard };
