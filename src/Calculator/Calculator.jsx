c75e405edc2034c700bb9f407aa20eaf015b80d9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-10-22 22:35:30 +0300

    Update Calculator.jsx

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -24,7 +24,7 @@ const Calculator = () => {
 
   const enterNKdata = (value) => {
     setResult("");
-    setCharacter(value);
+    setExpression(insertCharacter(cursorIndex, value));
   };
 
   const calculate = () => {
